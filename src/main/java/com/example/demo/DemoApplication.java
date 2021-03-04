ada60ec2f8d2654575231969b8d8bf7263f29e0d
liminchuan <478561991@qq.com>
2021-03-04 10:55:16 +0800

    去掉没用文件

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.core.io.support.SpringFactoriesLoader;
 @SpringBootApplication
 //也可用另外三个替代
 //@Configuration  //相当于把标注的类作为配置文件
-//@EnableAutoConfiguration  //帮助SpringBoot应用将所有符合条件的@Configuration配置都加载到当前SpringBoot，并创建对应配置类的Bean，并把该Bean实体交给IoC容器进行管理。
+//@EnableAutoConfiguration  //自动配置，帮助SpringBoot应用将所有符合条件的@Configuration配置都加载到当前SpringBoot，并创建对应配置类的Bean，并把该Bean实体交给IoC容器进行管理。
 //@ComponentScan  //定义扫描的路径从中找出标识了需要装配的类自动装配到spring的bean容器中
 public class DemoApplication {
     public static void main(String[] args) {
